06031248b5bc8d3e4f19f002157dc11269b46499
Ralf Bell <ralf.bell@ges-systemhaus.de>
2017-09-28 11:43:59 +0200

    Anpassungen

--- wetterstation/src/downloadAllSensors.py
+++ wetterstation/src/downloadAllSensors.py
@@ -9,6 +9,14 @@ import pandas as pd
 from builtins import range
 from asyncio.tasks import wait
 
+conn = mysql.connector.connect(**SQLCONFIG)
+cur = conn.cursor()
+
+downDate = '2017-09-25'
+downloads = 0
+start = time.time()
+
+#Hole Geodaten zu gegebenr Länge/Breite
 def get_geodata(row):
     url = 'http://nominatim.openstreetmap.org/reverse'
     lat = row['lat']
@@ -30,39 +38,39 @@ def get_geodata(row):
             row['adresse'] = ''
     row['plz'] = res.json()['address']['postcode']
 
-
-conn = mysql.connector.connect(**SQLCONFIG)
-cur = conn.cursor()
-
-url = 'http://archive.luftdaten.info/'  # starting url
-
-downDate = '2017-09-24'
-downloads = 0
-start = time.time()
-
-for i in range(4138,5403):
-    filename =  downDate + '_sds011_sensor_' + str(i) + '.csv'
-    sensorUrl = url + downDate + '/' + filename
+#Hole Datei vom luftdaten-Archich:
+def get_file(date, nr, typ = 'sds011'):
+    url = 'http://archive.luftdaten.info/'  # starting url
+    filename =  date + '_' + typ + '_sensor_' + nr + '.csv'
+    sensorUrl = url + date + '/' + filename
     
     # Download the page.
     try:
         r = requests.head(sensorUrl)
         r.raise_for_status()
     except:
-        continue
+        return
 
     sensor = pd.read_csv(sensorUrl, header=0, delimiter = ';')
     row = sensor.iloc[0]
-    print(row['sensor_id'], row['lon'], row['lat'])
     
     if row['lon'] >= 6.89 and row['lon'] <= 7.12  and row['lat'] >= 51.01 and row['lat'] <= 51.1:
         get_geodata(row)
-        cur.execute('''INSERT IGNORE into luftdaten_lev (sensor_id, sensor_type, location, lat, lon, adresse, plz, ort )       
+        save_data(row)
+
+
+def save_data(row):    
+    global downloads    
+    cur.execute('''INSERT IGNORE into luftdaten_lev (sensor_id, sensor_type, location, lat, lon, adresse, plz, ort )       
                        VALUES (%s,%s,%s, %s, %s, %s, %s, %s)''', (str(row['sensor_id']), row['sensor_type'], str(row['location']), float(row['lat']), float(row['lon']), row['adresse'], row['plz'], row['ort']))
         
-        conn.commit()
-        downloads += 1
-        wait(5)
+    conn.commit()
+    downloads += 1
+    wait(5)
+
+for i in range(5403, 5404):
+    get_file(downDate, str(i))
+    
 
 end = time.time()
 
--- wetterstation/src/downloadLuftdatenInfo.py
+++ wetterstation/src/downloadLuftdatenInfo.py
@@ -6,29 +6,44 @@ from urllib.request import urlretrieve
 import datetime
 from dateutil.rrule import rrule, DAILY
 import mysql.connector
-from keys import SQLCONFIG, SENSORLIST
+from keys import SQLCONFIG
 
 conn = sqlite3.connect('wetter.sqlite')
 cur = conn.cursor()
 con2 = mysql.connector.connect(**SQLCONFIG)
 cur2 = con2.cursor()
 
+cntsql = 0
+cntmys = 0
+cntdow = 0
+cntntf = 0
+
 
 def sqlite2mysql(cur, con2, cur2, start):
+    global cntmys
 # Als Select-Parameter für SQLITE muß eine Liste [] übergeben werden!
     for data in cur.execute('''select id, timestamp, sensor_id, sensor_type, location, 
                                 lat, lon, p1, p2, pressure, temperature, humidity
                                 from luftdaten_raw where date(timestamp) >= ?  ''', [start]):
-        print(data)
+#        print(data)
         cur2.execute('''INSERT IGNORE into luftdaten  
                         (timestamp, sensor_id, sensor_type, location, 
                         lat, lon, p1, p2, pressure, temperature, humidity) 
                         VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',
                         (data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11]))
+        cntmys += 1
     con2.commit()
+    
 
 def get_files(conn, cur, start, ende):
-    sensorList = SENSORLIST
+    global cntsql, cntdow, cntntf
+    #sensorList = SENSORLIST
+    cur2.execute('Select sensor_id, sensor_type from luftdaten_lev')
+    sensors = cur2.fetchall()
+    sensorList = []
+    for sensor in sensors:
+        sensorList.append('_' + str(sensor[1]).lower() + '_sensor_' + str(sensor[0]))
+        
     url = 'http://archive.luftdaten.info/'  # starting url
     localDir = '../data/'
     
@@ -41,12 +56,14 @@ def get_files(conn, cur, start, ende):
             dst = localDir + fileName
             sensorUrl = url + downDate + '/' + fileName  # Download the page.
             try:
-                if os.path.isfile(dst):
-                    print('%s already downloaded' % sensorUrl)
-                else:
-                    urlretrieve(sensorUrl, dst)
-                    print('Downloading page %s' % sensorUrl)
+                #if os.path.isfile(dst):
+                #    print('%s already downloaded' % sensorUrl)
+                #else:
+                urlretrieve(sensorUrl, dst)
+                #    print('Downloading page %s' % sensorUrl)
+                
                 with open(dst, newline='') as csvfile:
+                    cntdow += 1
                     reader = csv.DictReader(csvfile, delimiter=';', quotechar='|')
                     for row in reader:
                         try:
@@ -62,21 +79,23 @@ def get_files(conn, cur, start, ende):
                                 cur.execute('''INSERT or IGNORE into luftdaten_raw 
                                 (timestamp, sensor_id, sensor_type, location, lat, lon, p1, p2 )       
                                 VALUES (?,?,?,?,?,?,?,?)''', (row['timestamp'], row['sensor_id'], row['sensor_type'], row['location'], row['lat'], row['lon'], row['P1'], row['P2']))
+                            cntsql += 1
                         except:
                             print('Fehler: ', row)
             except:
                 print('File not found %s' % sensorUrl)
+                cntntf += 1
         conn.commit()
 
 # Letzten Eintrag aus MySQL-DB holen:
 cur2.execute('Select max( timestamp ) from luftdaten')
 start = cur2.fetchone()[0]
-#start = datetime.datetime.strptime('2017-09-24', "%Y-%m-%d")
+#start = datetime.datetime.strptime('2017-09-28', "%Y-%m-%d")
 ende = datetime.date.today()
 
 
 
-print(start)
+print('Starttimestamp: ', start)
 
 get_files(conn, cur, start, ende)
 
@@ -85,4 +104,4 @@ sqlite2mysql(cur, con2, cur2, start)
 con2.close()
 conn.close()
 
-print('Done.')
+print(cntdow, 'files downloded. ', cntntf, ' files not found.', cntsql, ' sets inserted in SQLITEDB. ', cntmys, ' sets inserted in MySQLDB.')
