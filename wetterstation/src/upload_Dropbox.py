b0e380a6f9977ba26cb62c3d3e39444b0adb33ac
Ralf Bell <ralf.bell@ges-systemhaus.de>
2017-12-07 09:53:37 +0100

    Dropboxupload as standalone module

---
+++ wetterstation/src/upload_Dropbox.py
@@ -0,0 +1,13 @@
+import dropbox, keys
+from datetime import date, timedelta
+
+dbx = dropbox.Dropbox(keys.DROPBOX_TOKEN)
+
+yesterday = date.today() - timedelta(1) 
+
+filename = yesterday.strftime("%Y%m%d") +  '.txt'
+file = keys.DIR + filename
+target = '/' + filename
+
+with open(file, 'rb') as f:
+        dbx.files_upload(f.read(), target)
