426d7cfdf8bb819197cd839892d19da86ae87dc0
Ralf Bell <ralf.bell@ges-systemhaus.de>
2018-03-06 07:28:35 +0100

    chmod +x

--- wetterstation/src/downloadAllSensors.py
+++ wetterstation/src/downloadAllSensors.py
@@ -1,4 +1,4 @@
-#! python3
+#!/usr/bin/env python3
 # downloadLuftdatenInfo.py - Downloads data from http://archive.luftdaten.info/ 
 
 from keys import SQLCONFIG
--- wetterstation/src/downloadLuftdatenInfo.py
+++ wetterstation/src/downloadLuftdatenInfo.py
@@ -1,4 +1,4 @@
-#! python3
+#!/usr/bin/env python3
 # downloadLuftdatenInfo.py - Downloads data from http://archive.luftdaten.info/ 
 
 import os, csv, sqlite3
