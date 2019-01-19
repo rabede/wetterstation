e3dd505208e892215f2c2d045850f86109bcd07b
rabe <ralf.bell@web.de>
2019-01-19 11:28:20 +0100

    checked for empty sensor-data

--- wetterstation/src/api.py
+++ wetterstation/src/api.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
-import requests
 import logging
-import keys
 
 from flask import Flask, request
 from flask_restful import abort, Api, fields, marshal_with, Resource
+import requests
 
+import keys
 from models import AirrohrModel, SoilMoistureModel
 import status
 
+
 logging.basicConfig(filename= keys.DIR + 'api.log', 
-                    level=logging.DEBUG,
+                    level=logging.WARN,
                     format='[%(asctime)s] %(levelname)s:%(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S %p')
                     
@@ -22,7 +23,8 @@ class AirrohrManager():
         self.measures = {}
 
     def insert_measure(self, measure):
-        logging.info(measure.sensor_id, ' empfangen')
+        logging.info(measure.sensor_id + ' empfangen')
+        logging.debug("Airrohr.inseart_measure: " + str(measure))
         try:
             if measure.sensor_id == '1497627':
                 requests.get("https://api.thingspeak.com/update?api_key=TSENL6QISWDJOSK2&field5=" + str(measure.p10)
@@ -31,9 +33,11 @@ class AirrohrManager():
             pass
        
     def get_measure(self, timestamp):
+        logging.debug("Airrohr.get_measure: " + str(self.measures))
         return self.measures[timestamp]
 
     def delete_measure(self, timestamp):
+        logging.debug("Airrohr.delete_measure: " + str(self.measures))
         del self.measures[timestamp]
 
 airrohr_fields = {
@@ -60,10 +64,12 @@ class Airrohr(Resource):
             
     @marshal_with(airrohr_fields)
     def get(self, timestamp):
+        logging.debug("Airrohr.get " + str(self.measures))
         self.abort_if_measure_doesnt_exist(timestamp)
         return airrohr_manager.get_measure(timestamp)
 
     def delete(self, timestamp):
+        logging.debug("Airrohr.delete: " + str(self.measures))
         self.abort_if_measure_doesnt_exist(timestamp)
         airrohr_manager.delete_measure(timestamp)
         return '', status.HTTP_204_NO_CONTENT
@@ -72,18 +78,33 @@ class AirrohrList(Resource):
 
     @marshal_with(airrohr_fields)
     def get(self):
+        logging.debug("AirrohrList.get: " + str(airrohr_manager.measures.values()))
         return [v for v in airrohr_manager.measures.values()]
 
     @marshal_with(airrohr_fields)
     def post(self):
         args = {}
         json_data = request.get_json(force=True)
+        logging.debug("AirrohrList.post: " + str(json_data))
         args['sensor_id'] = json_data['esp8266id']
         args['software_version'] = json_data['software_version']
         
         for sensor in json_data['sensordatavalues']:
-            # print(sensor['value_type'] + ': ' +  sensor['value'])
+            logging.info(sensor['value_type'] + ': ' +  sensor['value'])
             args[sensor['value_type']] = sensor['value']
+
+        try:
+            temperature=args['BME280_temperature']
+        except:
+            temperature=None
+        try:
+            humidity=args['BME280_humidity']
+        except:
+            humidity=None
+        try:
+            pressure=args['BME280_pressure']            
+        except:
+            pressure=None         
 
         measure = AirrohrModel(
             timestamp=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
@@ -91,10 +112,10 @@ class AirrohrList(Resource):
             software_version=args['software_version'],
             p10=args['SDS_P1'],
             p2=args['SDS_P2'],
-            temperature=args['BME280_temperature'],
-            humidity=args['BME280_humidity'],
-            pressure=args['BME280_pressure']
-            )
+            temperature=temperature,
+            humidity=humidity,
+            pressure=pressure
+        )
         airrohr_manager.insert_measure(measure) 
         return measure, status.HTTP_201_CREATED   
 
@@ -123,12 +144,14 @@ class MoistureList(Resource):
 
     @marshal_with(moisture_fields)
     def get(self):
+        logging.debug("MoistureList.get: " + str(moisture_manager.measures.values()))
         return [v for v in moisture_manager.measures.values()]
 
     @marshal_with(moisture_fields)
     def post(self):
         args = {}
         json_data = request.get_json(force=True)
+        logging.debug("MoistureList.post: " + str(json_data))
         args['sensor_id'] = json_data['esp8266id']
         args['software_version'] = json_data['software_version']
         
