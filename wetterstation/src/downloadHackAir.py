813cc10dea29445a99e9b7207b2528fcb0f102d0
Ralf Bell <ralf.bell@ges-systemhaus.de>
2019-03-18 08:44:14 +0100

    andere HackAir-Parameter

--- wetterstation/src/downloadHackAir.py
+++ wetterstation/src/downloadHackAir.py
@@ -6,17 +6,17 @@ import requests
 url = "?timestampStart=2018-09-30&source=sensors_arduino&sensor=348&show=all"
 
 url = 'https://api.hackair.eu/measurements'
-timestampStart = '2018-09-27'
+timestampStart = '2018-09-21'
 timestampEnd = ''
 location = '7.0,51.0|7.1,51.1'
-source = 'sensors_arduino, sensors_bleair'
+source = 'sensors_arduino'
 sensor = '348' #'889'
-user = ''
+user = '393'
 pollutant = ''
 #type = '' #Response type
 show = 'all'  # Defines how measurements are returned, specifically sensor measurements. Use: 'latest' for the latest measurements, 'hourly_averages' for hourly averages or 'all' for all measurements. Default: 'latest
 
-params = {'timestampStart':timestampStart, 'location':location, 'source':source, 'format':'json'}
+params = {'timestampStart':timestampStart, 'source':source, 'user': user, 'show': show, 'format': json}
 
 res = requests.get(url, params=params)
 js = res.json()
